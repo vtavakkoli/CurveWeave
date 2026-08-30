16475f953fe59b72267bff7cba20dbd9d5140228
Vahid Tavakkoli <vtavakkoli@yahoo.com>
2026-08-30 14:08:34 +0200

    chore: cache editor enhancement module

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'curveweave-v1';
-const ASSETS = ['./','./index.html','./styles.css','./src/app.js','./src/svg-utils.js','./assets/icon.svg','./manifest.webmanifest'];
+const CACHE = 'curveweave-v2';
+const ASSETS = ['./','./index.html','./styles.css','./src/app.js','./src/svg-utils.js','./src/enhancements.js','./assets/icon.svg','./manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
