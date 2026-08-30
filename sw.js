216345b9bef29af148371b110c8fc80b76d77112
Vahid Tavakkoli <vtavakkoli@yahoo.com>
2026-08-30 21:48:56 +0200

    chore: cache Pro Vector Studio offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'curveweave-v3';
-const ASSETS = ['./','./index.html','./styles.css','./src/app.js','./src/svg-utils.js','./src/enhancements.js','./src/selection-utils.js','./src/advanced-selection.js','./assets/icon.svg','./manifest.webmanifest'];
+const CACHE = 'curveweave-v4';
+const ASSETS = ['./','./index.html','./styles.css','./src/app.js','./src/svg-utils.js','./src/enhancements.js','./src/selection-utils.js','./src/advanced-selection.js','./src/path-geometry.js','./src/boolean-utils.js','./src/pro-vector.js','./assets/icon.svg','./manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
